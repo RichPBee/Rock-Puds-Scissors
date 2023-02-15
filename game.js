2db588e035f006ab615714a17259e78c2e5b28d7
Lauren <lauren.boyce93@gmail.com>
2023-02-15 15:09:32 +0000

    buttons

---
+++ game.js
@@ -0,0 +1,14 @@
+const rockBtn = document.getElementById('rock');
+const display = document.querySelector('div2');
+
+rockBtn.addEventListener('click', () => {
+    display.innerHTML = 'Frarg';
+});
+
+
+const paperBtn = document.getElementById('paper');
+
+
+
+
+const scissorBtn = document.getElementById('scissors')
