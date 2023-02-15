b8efa50a9fa45f19f248a4937d314a9ed56d9b38
Lauren <lauren.boyce93@gmail.com>
2023-02-15 19:11:12 +0000

    another commit

--- game.js
+++ game.js
@@ -1,14 +1,36 @@
-const rockBtn = document.getElementById("rock");
 const display = document.querySelector('.div2');
+let playerChoice = '';
 
+const rockBtn = document.getElementById("rock");
 rockBtn.addEventListener('click', () => {
-    display.innerHTML = 'Frarg';
+    display.innerHTML = 'You are a rock';
+    playerChoice = 'rock'
+    roundOfPlay(playerChoice, getComputerChoice())
 });
 
-
 const paperBtn = document.getElementById('paper');
-
-
-
+paperBtn.addEventListener('click', () => {
+    display.innerHTML = 'You are a piece of paper';
+    playerChoice = 'paper'
+    roundOfPlay(playerChoice, getComputerChoice())
+})
 
 const scissorBtn = document.getElementById('scissors')
+scissorBtn.addEventListener('click', () => {
+    display.innerHTML = 'You are a pair of scissors';
+    playerChoice = 'scissors'
+    roundOfPlay(playerChoice, getComputerChoice())
+})
+
+const getComputerChoice = () => {
+    const options = ['rock', 'paper', 'scissors'];
+    let chosenNumber = Math.floor(Math.random() * 2);
+    let computerChoice = options[chosenNumber]
+    return computerChoice;
+}
+
+const roundOfPlay = (choice1, choice2) => {
+    if (choice1 === choice2) {
+        return "It's a draw"
+    }  
+}
